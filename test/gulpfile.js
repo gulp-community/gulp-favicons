e2287879de2d041c3f119dd3c869ec10ed903d1c
Julian Grinblat <julian@dotcore.co.il>
2023-03-27 10:00:06 +0900

    fix: correctly compose streams

--- test/gulpfile.js
+++ test/gulpfile.js
@@ -16,6 +16,7 @@ export default () =>
             version: '1.0',
             logging: true,
             online: false,
+            pipeHTML: true,
             icons: {
                 coast: false
             },
